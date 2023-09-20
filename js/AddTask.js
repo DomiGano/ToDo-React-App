9b08b840f049391e1f640d15caffa38f7152d2a8
DomiGano <ganowiczdominik@gmail.com>
2023-09-20 21:56:57 +0200

    Add basic validation and Error message

--- js/AddTask.js
+++ js/AddTask.js
@@ -4,6 +4,7 @@ import { API_KEY, API_URL } from "../api/constants";
 export const AddTask = ({setTasks}) => {
     const [title, setTitle] = useState("")
     const [description, setDescription] = useState("")
+    const [error, setError] = useState("")
 
     const clearValues = () => {
         setDescription("")
@@ -12,6 +13,13 @@ export const AddTask = ({setTasks}) => {
 
     const newTask = () => {
         const data = {title: title, description: description, status: false}
+
+        if(title.length < 2) {
+          setError("Tytuł powinien zawierać minimum 2 znaki")
+          console.log(error)
+        } else {
+          setError("")
+          console.log(error)
 
         fetch(`${API_URL}/tasks`, {
             method: "POST",
@@ -29,15 +37,21 @@ export const AddTask = ({setTasks}) => {
             })
             .catch((err) => console.log(err));
         };
+      }
             
       
 
     return (
+      <>
         <div className="addTask">
             <h3 className="addTask__title">Nowe Zadanie</h3>
             <input className="addTask__input addTask__input-title" value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Tytuł"></input>
             <input className="addTask__input addTask__input-description" value={description} onChange={(e) => setDescription(e.target.value)} type="text" placeholder="Opis"></input>
-            <button className="addButton" onClick={title !== "" && description !== "" ? () => {newTask()} : null}>Dodaj zadanie <i className="fa-solid fa-circle-plus"></i></button>
+            <button className="addButton" onClick={() => newTask()}>Dodaj zadanie <i className="fa-solid fa-circle-plus"></i></button>
         </div>
+        <div>
+          <h4 style={error == "" ? {display: "none"} : {display: "block"}}>Błąd: {error}</h4>
+        </div>
+        </>
     )
 }
